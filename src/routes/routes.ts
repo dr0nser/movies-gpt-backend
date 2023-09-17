495bd7f5a60c871c883152c80f2b62d8d1f7900d
whyucode <dassouvik14@outlook.com>
2023-09-17 23:24:55 +0530

    implemented /banner endpoint

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -12,26 +12,49 @@ dotenv.config();
 
 const router = express.Router();
 
-router.get("/gallery", async (req: Request, res: Response) => {
-  const userId: string = req.query.userId.toString();
+router.get("/banner", async (req: Request, res: Response) => {
+  const userId: string = req.query.userId?.toString();
+  if (!userId) {
+    return res.status(400).json({ error: "userId is required" });
+  }
   try {
     const user = await getAuth().getUser(userId);
-    if (user) {
-      const nowPlaying: MovieResponse[] = await getNowPlaying();
-      const topRated: MovieResponse[] = await getTopRated();
-      const popular: MovieResponse[] = await getPopular();
-      const upcomingMovies: MovieResponse[] = await getUpcoming();
-
-      const result: GalleryResponse[] = [
-        { name: "Trending", data: nowPlaying },
-        { name: "Top Rated", data: topRated },
-        { name: "Popular", data: popular },
-        { name: "Upcoming", data: upcomingMovies },
-      ];
-      res.status(200).json(result);
-    }
+    if (!user) return res.status(403).json({ error: "User not found" });
+    const nowPlaying: MovieResponse[] = await getNowPlaying();
+    const result = nowPlaying.filter(
+      (movie: MovieResponse) => movie.trailerUrl !== null
+    );
+    if (result.length === 0)
+      return res.status(404).json({ error: "No movies found" });
+    res.status(200).json(result[0]);
   } catch (error) {
-    res.status(404).json(error);
+    console.error(error);
+    res.status(500).json({ error: "Unexpected error occurred" });
+  }
+});
+
+router.get("/gallery", async (req: Request, res: Response) => {
+  const userId: string = req.query.userId?.toString();
+  if (!userId) return res.status(400).json({ error: "userId is required" });
+  try {
+    const user = await getAuth().getUser(userId);
+    if (!user) return res.status(403).json({ error: "User not found" });
+    const [nowPlaying, topRated, popular, upcomingMovies] = await Promise.all([
+      getNowPlaying(),
+      getTopRated(),
+      getPopular(),
+      getUpcoming(),
+    ]);
+    const result: GalleryResponse[] = [
+      { name: "Trending", data: nowPlaying },
+      { name: "Top Rated", data: topRated },
+      { name: "Popular", data: popular },
+      { name: "Upcoming", data: upcomingMovies },
+    ];
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Unexpected error occurred" });
   }
 });
 
